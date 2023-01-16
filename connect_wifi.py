ac5d3391ee9ac43f447795b051220681643827f0
Trung Hoang Nguyen <83159640+htrungngx@users.noreply.github.com>
2023-01-16 23:02:33 +0200

    Setting up Wifi connection

--- connect_wifi.py
+++ connect_wifi.py
@@ -1,11 +1,11 @@
 import network #Import library
-sta_if = network.WLAN(network.STA_IF)
+sta_if = network.WLAN(network.STA_IF) 
 
 
 def connectWifi():
-    if sta_if.isconnected() == False:
-        sta_if.active(True)
-        sta_if.connect('AIoT-Garage', 'asdfghjk') #Enter SSID and Wifi Password
+    if sta_if.isconnected() == False: #If network is not connected
+        sta_if.active(True) #Reactive Network
+        sta_if.connect('AIoT-Garage', 'asdfghjk') #Enter SSID and Wifi Password 
         print('connection is established for the first time: ' + str(sta_if.isconnected())) #Print if wifi is connected
     else:
         print('Wifi already connected')
